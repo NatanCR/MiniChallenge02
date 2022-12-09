f43a03a251411c84e8d748dca038cebbe9a504be
Natan Camargo Rodrigues <natanrodrigues0044@gmail.com>
2022-12-08 22:39:16 -0300

    correcoes

--- NossoMeio/NossoMeio/GameObjects/JunkObject.swift
+++ NossoMeio/NossoMeio/GameObjects/JunkObject.swift
@@ -78,18 +78,3 @@ class JunkObject: SKNode {
         self.actionEndMoved?(touches)
     }
 }
-
-extension SKSpriteNode {
-    func aspectFillToSize(fillSize: CGSize) {
-        if let texture = self.texture {
-            self.size = texture.size()
-            
-            let verticalRatio = fillSize.height / self.texture!.size().height
-            let horizontalRatio = fillSize.width /  self.texture!.size().width
-            
-            let scaleRatio = horizontalRatio > verticalRatio ? horizontalRatio : verticalRatio
-            
-            self.setScale(scaleRatio)
-        }
-    }
-}
